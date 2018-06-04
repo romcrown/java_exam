1b963b1f1653cb6daea526193067a460bcbc8b52
GSR <tmdfuf8460@gmail.com>
2018-06-04 19:03:02 +0900

    commit 2

--- src/java_world.java
+++ src/java_world.java
@@ -1,6 +1,7 @@
 
 public class java_world {
 	public static void main(String[] args) {
-		System.out.println("hell java");
+		System.out.println("hello java");
+		System.out.println("java world");
 	}
 }
