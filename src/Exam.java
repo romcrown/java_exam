7738f37f6a5ad8fe0a61d012ccd28456971b3e5a
GSR <tmdfuf8460@gmail.com>
2018-06-06 19:32:00 +0900

    exam F

--- src/Exam.java
+++ src/Exam.java
@@ -4,7 +4,7 @@ public class Exam {
 	
 	public static void main(String[] args) {
 	
-		System.out.println("값을 입력해 주세요(ex 8,9).");
+		System.out.printf("숫자를 입력해 주세요(ex 8,9 = 2x1,.. ,2x9, ..8x1,.. 8x9,.. ) : ");
 		Scanner sc = new Scanner(System.in);
 		//int a = sc.nextInt(); int형을 사용할떄 
 		String Input = sc.nextLine(); // String 을 사용할때.
@@ -14,8 +14,9 @@ public class Exam {
 		int Second = Integer.parseInt(Splite[1]);
 	
 		for(int a = 2 ; a <= First ; a++) {
+			System.out.println(a+"단");
 			for(int b = 1 ; b <= Second ; b++ ) {
-				 System.out.println(a*b);
+				 System.out.println(a+" x "+b+" = "+a*b);
 			}
 			
 		}
